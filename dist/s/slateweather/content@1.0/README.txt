4770cb998c48a6d6a9b1419b80052157710f42d8
plopilpy <plopilpy@gmail.com>
2022-06-13 13:00:04 -0400

    everything ever

---
+++ dist/s/slateweather/content@1.0/README.txt
@@ -0,0 +1,3 @@
+SlateWeather Copyright (c) 2022 Plopilpy.
+
+Please do not redistribute or modify code.
---
+++ dist/s/slateweather/content@1.0/scripts/install.js
@@ -0,0 +1,2 @@
+// Ran upon package installation
+w96.sys.execCmd(`internete`, 'c:/local/slateweather/install.html')
