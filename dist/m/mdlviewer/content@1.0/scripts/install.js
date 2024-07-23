cad683ceb4bed7ef1c750da3863ec724d1ef7cd6
patrykstan <plopilpy@tazzy.ca>
2024-07-23 18:58:00 -0400

    install js

--- dist/m/mdlviewer/content@1.0/scripts/install.js
+++ dist/m/mdlviewer/content@1.0/scripts/install.js
@@ -1,3 +1,6 @@
+async function mdlAddEtc() {
 let assocs = JSON.parse(await w96.FS.readstr("C:/system/etc/apps.json"));
 assocs.push({ "name": "Model Viewer", "icon": "C:/local/mdlviewer/icon.png", "exec": "model-viewer", "assoc": ["glb", "gltf"] });
 await w96.FS.writestr("C:/system/etc/apps.json", JSON.stringify(assocs, null, 4));
+}
+mdlAddEtc();
